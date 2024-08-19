b8c10297e6a4ad29a18ce762104c8de9ae0e61a8
Zapdos750 <zapdos750@gmail.com>
2024-08-19 04:35:16 -0700

    Change Menu Font and add pause screen

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -8,6 +8,9 @@ public class PlayerManager : MonoBehaviour
     public GameObject largePlayer;
     public CameraFollow cameraFollow; // Reference to the CameraFollow script
 
+    public GameObject canvas;
+    public bool isPaused = false;
+
     private GameObject currentPlayer;
 
     private void Start()
@@ -21,6 +24,35 @@ public class PlayerManager : MonoBehaviour
             cameraFollow.target = normalPlayer.transform; // Set initial target
         }
     }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!isPaused)
+            {
+                PauseGame();
+            }
+            else
+            {
+                ResumeGame();
+            }
+        }
+    }
+
+    public void PauseGame()
+    {
+        canvas.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void ResumeGame()
+    {
+        canvas.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }    
 
     public void ChangeSize(GameObject newPlayer, float verticalOffset)
     {
