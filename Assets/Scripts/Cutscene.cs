92ccfcaae0a0ac3a0fa99e97002f00b41ef50511
Zapdos750 <zapdos750@gmail.com>
2024-08-20 00:09:01 -0700

    add text to final scene

--- Assets/Scripts/Cutscene.cs
+++ Assets/Scripts/Cutscene.cs
@@ -1,10 +1,36 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Cutscene : MonoBehaviour
 {
+    public GameObject button;
     public GameObject canvas;
     public GameObject player;
+    public GameObject goodbye;
     public Animator animator;
+
+    public float targetTime = 7.0f;
+
+    void Update()
+    {
+
+        targetTime -= Time.deltaTime;
+
+        if (targetTime <= 3.0f)
+        {
+            goodbye.SetActive(true);
+        }
+
+        if (targetTime <= 0.0f)
+        {
+            timerEnded();
+        }
+    }
+
+    void timerEnded()
+    {
+        button.SetActive(true);
+    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -14,6 +40,6 @@ public class Cutscene : MonoBehaviour
 
     public void Done()
     {
-        animator.SetTrigger("cutscene");
+        SceneManager.LoadSceneAsync("Main Menu");
     }
 }
